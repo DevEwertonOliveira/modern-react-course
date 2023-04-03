18b859408eebc825ff5ac874ce626e4c47d13f61
DevEwertonOliveira <ewerton2007@gmail.com>
2023-04-03 11:08:07 -0400

    image for props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard({title, handle}) {
-  console.log(title, handle)
+function ProfileCard({title, handle,image}) {
+  
 
   return (
     <div>
+      <img src={image} alt="pda logo"/>
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
